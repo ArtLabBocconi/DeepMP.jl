327941c1f1d2745225ab3b48a623b47f3a815fcb
Carlo Lucibello <carlo.lucibello@gmail.com>
2020-09-09 16:22:02 +0200

    stub for focusing BP

--- src/DeepMP.jl
+++ src/DeepMP.jl
@@ -89,21 +89,28 @@ mutable struct FactorGraph
 end
 
 mutable struct ReinfParams
-    r::Float64
+    r::Float64         # reinforcement for W variables
     rstep::Float64
-    ry::Float64
+    ry::Float64         # reinforcement for Y variables
     rystep::Float64
-    ψ::Float64
+    y::Float64          # parameter for FocusingBP
+    ψ::Float64          # damping parameter
     wait_count::Int
-    ReinfParams(r=0., rstep=0., ry=0., rystep=0., ψ=0.) = new(r, rstep, ry, rystep, ψ, 0)
+    ReinfParams(r=0., rstep=0., ry=0., rystep=0., ψ=0., y=0) = new(r, rstep, ry, rystep, ψ, y, 0)
 end
 
 function update_reinforcement!(reinfpar::ReinfParams)
     if reinfpar.wait_count < 10
         reinfpar.wait_count += 1
     else
-        reinfpar.r = 1 - (1-reinfpar.r) * (1-reinfpar.rstep)
-        reinfpar.ry = 1 - (1-reinfpar.ry) * (1-reinfpar.rystep)
+        if reinfpar.y <= 0
+            # reinforcement update
+            reinfpar.r = 1 - (1-reinfpar.r) * (1-reinfpar.rstep)
+            reinfpar.ry = 1 - (1-reinfpar.ry) * (1-reinfpar.rystep)
+        else
+            # focusing update
+            @assert false #TODO
+        end
     end
 end
 
