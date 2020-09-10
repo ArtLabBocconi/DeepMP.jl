48582dad6fc588fa0e2c2cd2f1c1605dccb0fd3f
Carlo Lucibello <carlo.lucibello@gmail.com>
2020-09-10 12:50:30 +0200

    add back perceptron tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,9 @@
 using DeepMP
 using Test, DelimitedFiles, Random, Statistics
 
-# @testset "Perceptron" begin
-#     include("perceptron.jl")
-# end
+@testset "Perceptron" begin
+    include("perceptron.jl")
+end
 
 @testset "MLP" begin
     include("mlp.jl")
