f15345c8dac89a816896553d4e74004b9e9323c1
Carlo Lucibello <carlo.lucibello@gmail.com>
2020-10-21 17:30:43 +0200

    fix test

--- test/sparsity.jl
+++ test/sparsity.jl
@@ -2,9 +2,7 @@ y = Int.(readdlm(@__DIR__() * "/../fmnist/seed7/Y.txt")) |> vec
 X = readdlm(@__DIR__() * "/../fmnist/seed7/X.txt")
 
 @time g, W, teacher, E = DeepMP.solve(X,y, K=[784,5, 1]
-                   , layers=[:bpacc, :bpex]
-                   ,r=.9,rstep=0.01, seed=2,maxiters=500, density=0.5);
+                   , layers=[:bpacc, :bpex] , verbose=0
+                   , r=.9,rstep=0.01, seed=2,maxiters=500, density=0.5);
 
-
-@test E < 5
-@test_broken E == 0
+@test E == 0
