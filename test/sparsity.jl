28e1f47483251ef4a8cc637923b12606c7f567f0
Carlo Lucibello <carlo.lucibello@gmail.com>
2020-10-26 12:00:42 +0100

    unbroke test'

--- test/sparsity.jl
+++ test/sparsity.jl
@@ -6,5 +6,4 @@ X = readdlm(@__DIR__() * "/../fmnist/seed7/X.txt")
                    , r=.9,rstep=0.01, seed=2,maxiters=500, density=0.5);
 
                 
-@test_broken E == 0
-@test E < 5
+@test E == 0
