3a35371d43e8610fc8ca989b232d5f80dec92c9a
Fabrizio Pittorino <fabrizio.pittorino@gmail.com>
2021-04-08 09:07:32 +0200

    choose gpu

--- scripts/mnist.jl
+++ scripts/mnist.jl
@@ -38,7 +38,7 @@ function get_mnist(M=60000; classes=[], seed=17, fashion=false)
     return xtrain, ytrain, xtest, ytest
 end
 
-function run_experiment(i; M=100, batchsize=1, K = [28*28, 101, 101, 1], usecuda=false)
+function run_experiment(i; M=100, batchsize=1, K = [28*28, 101, 101, 1], usecuda=false, gpu_id=0)
     if i == 7
         #@testset "SBP on MLP" begin
 
@@ -49,7 +49,7 @@ function run_experiment(i; M=100, batchsize=1, K = [28*28, 101, 101, 1], usecuda
 
         g, w, teacher, E, it = DeepMP.solve(xtrain, ytrain;
             xtest, ytest,
-            usecuda,
+            usecuda, gpu_id,
             K = K,
             seed = 1,
             maxiters = 1,
