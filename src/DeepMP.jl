6ea3842e229db3c0bdde6cb35fd877141765ed01
gabrieleperugini <gabriele.perugini@gmail.com>
2021-09-26 00:05:20 +0200

    add c.l. solve

--- src/DeepMP.jl
+++ src/DeepMP.jl
@@ -288,4 +288,157 @@ function solve(xtrain::AbstractMatrix, ytrain::AbstractVector;
     return g, getW(g), teacher, Etrain, it
 end
 
+function solveCL(xtrain::AbstractMatrix, ytrain::AbstractVector;
+                num_tasks = 6,
+                xtest = nothing, ytest = nothing,
+                dataset = :fashion,
+                K::Vector{Int},                # List of widths for each layer, e.g. [28*28, 101, 101, 1]
+                layers,                        # List of layer types  e.g. [:bpi, :bpi, :argmax],
+                maxiters = 100,
+                ϵ = 1e-4,                      # convergence criterion
+                r = 0., rstep = 0.,            # reinforcement parameters for W vars
+                ψ = 0.,                        # damping coefficient
+                yy = -1.,                      # focusing BP parameter
+                h0 = nothing,                  # external field
+                ρ = 1.,                        # coefficient for external field from mini-batch posterior
+                rbatch = 0.,                   # reinforcement parameter for external field
+                freezetop = false,             # freeze top-layer's weights to 1
+                teacher = nothing,
+                altsolv::Bool = true,
+                altconv::Bool = false,
+                seed::Int = -1,
+                β = Inf,
+                density = 1f0,                  # density of fully connected layer
+                batchsize = -1,                 # only supported by some algorithms
+                epochs = 100,
+                ϵinit = 0.,
+                plotinfo = 0,
+                verbose = 1,
+                usecuda = true,
+                gpu_id = -1,
+                saveres = false,
+                exp_folder=""
+                )
+
+    usecuda = CUDA.functional() && usecuda
+    device =  usecuda ? gpu : cpu
+    usecuda && gpu_id >= 0 && device!(gpu_id)
+    if seed > 0
+        Random.seed!(seed)
+        usecuda && CUDA.seed!(seed)
+    end
+
+    L = length(K) - 1
+    ψ = num_to_vec(ψ, L)
+    ρ = num_to_vec(ρ, L)
+
+    xtrain, ytrain = device(xtrain), device(ytrain)
+    xtest, ytest = device(xtest), device(ytest)
+    dtrain = DataLoader((xtrain, ytrain); batchsize, shuffle=true, partial=false)
+
+    g = FactorGraph(first(dtrain)..., K, ϵinit, layers; β, density, device)
+    h0 !== nothing && set_external_fields!(g, h0; ρ, rbatch);
+    if teacher !== nothing
+        teacher = device.(teacher)
+        has_same_size(g, teacher) && set_weight_mask!(g, teacher)
+    end
+    initrand!(g)
+    freezetop && freezetop!(g, 1)
+    reinfpar = ReinfParams(r, rstep, yy, ψ)
+
+    if saveres
+        res_folder = "results/" * exp_folder * "/"
+        ispath(res_folder) || mkpath(res_folder)
+
+        infolayers = string([string(layers[i], K[i+1], "_") for i = 1:length(layers)]...)
+        resfile =  "res_cl_nt$(num_tasks)_$(K[1])_" * infolayers
+        resfile *= "bs$(batchsize)_rho$(ρ)_r$(r)_damp$(ψ)"
+        density < 1 && (resfile *= "_density$(density)")
+        resfile *= "_M$(length(ytrain))_ϵinit$(ϵinit)_maxit$(maxiters)_ep$(epochs)"
+        resfile *= "_seed$(seed).dat"
+        fres = open(res_folder * resfile, "w")
+    end
+
+    N = size(xtrain, 1)
+    perms = [randperm(N) for _ = 1:num_tasks]
+
+    function errors(g, x, y)
+        mean(vec(forward(g, x)) .!= y)
+    end
+    function report(epoch; t=(@timed 0), converged=0., solved=0., meaniters=0.)
+        outf = @sprintf("%i", epoch)
+        for k = 1:num_tasks
+            outf *= @sprintf(" %g %g", errors(g, device(xtrain[perms[k],:]), device(ytrain)),
+                                      errors(g, device(xtest[perms[k],:]), device(ytest)))
+        end
+
+        Etrain = mean(vec(forward(g, xtrain)) .!= ytrain) * 100
+        Etrain_bayes = bayesian_error(g, xtrain, ytrain) *100
+        num_batches = length(dtrain)
+        Etest = 100.0
+        if ytest !== nothing
+            Etest = mean(vec(forward(g, xtest)) .!= ytest) * 100
+            Etest_bayes = bayesian_error(g, xtest, ytest) *100
+        end
+
+        verbose >= 1 && @printf("Epoch %i (conv=%g, solv=%g <it>=%g): Etrain=%.2f%% Etest=%.2f%%  r=%g rstep=%g ρ=%s  t=%g (layers=%s, bs=%d)\n",
+                                epoch, (converged/num_batches), (solved/num_batches), (meaniters/num_batches),
+                                Etrain, Etest, reinfpar.r, reinfpar.rstep, ρ, t.time, "$layers", batchsize)
+
+        verbose >= 1 && @printf("\t\t\tEtrainBayes=%.2f%% EtestBayes=%.2f%%\n", Etrain_bayes, Etest_bayes)
+
+        q0s, qWαβs = plot_info(g, 0; verbose)
+
+        if saveres
+            # outf = @sprintf("%d %g %g", epoch, Etrain, Etest)
+            # for (q0, qWαβ) in zip(q0s, qWαβs)
+            #     outf *= @sprintf(" %g %g", mean(q0), mean(qWαβ))
+            # end
+            # outf *= @sprintf(" %g", t.time)
+            # outf *= @sprintf(" %g %g", Etrain_bayes, Etest_bayes)
+            println(fres, outf); flush(fres)
+        end
+        return Etrain
+    end
+
+    for n = 1:num_tasks
+        xp, yp = device(xtrain[perms[n],:]), device(ytrain)
+        xpt, ypt = device(xtest[perms[n], :]), device(ytest)
+        dtrain = DataLoader((xp, yp); batchsize, shuffle=true, partial=false)
+
+        report(0)
+        for epoch = 1:epochs
+            converged = solved = meaniters = 0
+            t = @timed for (b, (x, y)) in enumerate(dtrain)
+                all(x->x==0, ρ) || set_Hext_from_H!(g, ρ, rbatch)
+                set_input_output!(g, x, y)
+
+                it, e, δ = converge!(g; maxiters, ϵ,
+                                        reinfpar, altsolv, altconv, plotinfo=0,
+                                        teacher, verbose=verbose-1)
+                converged += (δ < ϵ)
+                solved    += (e == 0)
+                meaniters += it
+
+                verbose >= 2 && print("b = $b / $(length(dtrain))\r")
+
+            end
+            Etrain = report(epoch; t, converged, solved, meaniters)
+        end
+    end
+    if saveres
+        close(fres)
+        println("outfile: $resfile")
+        conf_file = "results/conf$(resfile[12:end-4]).jld2"
+        @show conf_file
+        #save(conf_file, Dict("weights" => getW(g)))
+    end
+
+    Etrain = sum(vec(forward(g, xtrain)) .!= ytrain)
+    # return g, getW(g), teacher, Etrain, it, conf_file
+    return g, getW(g), teacher, Etrain
+end
+
+
+
 end #module
