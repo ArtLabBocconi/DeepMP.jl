afa70eb443f7c602fff1b54ac8435bdf3032d88a
Fabrizio Pittorino <fabrizio.pittorino@gmail.com>
2021-11-17 11:54:32 +0100

    scripts fig1

--- scripts/run_experiments_fig1.jl
+++ scripts/run_experiments_fig1.jl
@@ -1,16 +1,16 @@
-multicl = [false, true]
-datasets = [:mnist, :fashion, :cifar10]
-lays = [:bpi]: gpu_id = 0
-#lays = [:tap, :mf]; gpu_id = 1
+multicl = [true]
+datasets = [:fashion]
+lays = [:bpi, :tap, :mf]
 seeds = [2, 7, 11]
+gpu_id = 2
 
-K = [0, 1024, 1024, 1024, 0]
-ρ = [1.0, 1.0, 1.0, 0.9]
-ψ = [0.8, 0.8, 0.8, 0.8]
+Ks = [[0, 101, 101, 0], [0, 501, 501, 501, 0]]
+ρs = [[1.0, 1.0, 0.9], [1.0, 1.0, 1.0, 0.9]]
+ψs = [[0.8, 0.8, 0.8], [0.8, 0.8, 0.8, 0.8]]
 
 density = 1.0
 
-for multiclass in multicl, dataset in datasets, lay_type in lays, seed in seeds
+for multiclass in multicl, dataset in datasets, (K, ρ, ψ) in zip(Ks, ρs,  ψs), lay_type in lays, seed in seeds
     #try
         run_experiment(; multiclass, dataset, lay_type, seed, ρ, density,
         epochs=200, batchsize=128, usecuda=true, gpu_id, ψ, 
--- scripts/run_experiments_ultimate.jl
+++ scripts/run_experiments_ultimate.jl
@@ -2,15 +2,15 @@ multicl = [true]
 datasets = [:fashion]
 lays = [:bpi, :tap, :mf]
 seeds = [2, 7, 11]
-gpu_id = 1
+gpu_id = 0
 
-K = [0, 2048, 2048, 2048, 0]
-ψ = [0.8, 0.8, 0.8, 0.8]
+K = [0, 1024, 1024, 1024, 1024, 1024, 0]
+ψ = [0.8, 0.8, 0.8, 0.8, 0.8, 0.8]
 
 density = 1.0
 
 for multiclass in multicl, dataset in datasets, lay_type in lays, seed in seeds
-    ρ = [1.0, 1.0, 1.0, 0.9]
+    ρ = [1.0, 1.0, 1.0, 1.0, 1.0, 0.9]
     #try
         run_experiment(; multiclass, dataset, lay_type, seed, ρ, density,
         epochs=200, batchsize=128, usecuda=true, gpu_id, ψ, 
