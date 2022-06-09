398c4517d18d4c3d99af8c27b0f7d40553ae773f
Carlo Lucibello <carlo.lucibello@gmail.com>
2022-06-09 14:31:28 +0200

    fix to OutputLayer

--- src/layers/output.jl
+++ src/layers/output.jl
@@ -9,13 +9,15 @@ end
 function OutputLayer(y::AbstractVector; β=Inf)
     @assert β >= 0.
     B = β .* reshape(y, 1, :)
-    return OutputLayer(-1, y, B, β)
+    A = fill!(similar(B), β)
+    return OutputLayer(-1, y, B, A, Float32(β))
 end
 
 @functor OutputLayer
 
 function set_output!(lay::OutputLayer, y)
     lay.B .= lay.β .* reshape(y, 1, :)
+    lay.A .= lay.β .* fill!(similar(lay.B), β)
     lay.y = y
 end
 
